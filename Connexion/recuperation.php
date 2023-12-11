22d3f652cce478fcd31e0d059d586e7138d4a726
Hermann.M <marzeliereherman@gmail.com>
2023-12-11 10:36:05 +0100

    Salut nathan, j'ai changé mon recuperation.php maintenant il récupere les entrée de l'utilisateur (mail;mdp)

--- Connexion/recuperation.php
+++ Connexion/recuperation.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "mail : ".$POST['mail']."<br>"."mdp : ".$POST['mdp'];
-}
+    $mail = $_POST ['mail'];
+    $mdp = $_POST ['mdp'];
+
 ?>
